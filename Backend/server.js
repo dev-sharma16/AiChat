6fbf42212a16bced8ac95707992a45464835d1aa
Dev Sharma <devsharma1683@gmail.com>
2025-08-13 21:18:13 +0530

    moved socket io server setup in a seperate file

--- Backend/server.js
+++ Backend/server.js
@@ -1,33 +1,23 @@
 require('dotenv').config();
 const app = require('./src/app')
-const { createServer } = require("http");
-const { Server } = require("socket.io");
+// const { createServer } = require("http");
+// const { Server } = require("socket.io");
+const http = require('http')
+const { setupSocketServer }  = require('./src/socket/socket.server')
 const { setupChatRoutes } = require('./src/routes/chat.routes');
 const MCPTools = require('./src/service/mcp.service');
 const connectDb = require('./src/db/db')
 
 connectDb();
 
-const httpServer = createServer(app);
+// const httpServer = createServer(app);
 const mcpTools = new MCPTools();
 
-// Environment-based CORS configuration
-const allowedOrigins = [
-    "http://localhost:5173", // Local development
-    "https://ai-chat-sandy-kappa.vercel.app", // actual Vercel URL
-    process.env.FRONTEND_URL // Environment variable for production
-].filter(Boolean); // Remove undefined values
+//* setting up Socet.io sever
+const httpServer = http.createServer(app)
+const io = setupSocketServer(httpServer)
 
-const io = new Server(httpServer, { 
-    cors: {
-        origin: allowedOrigins,
-        methods: ["GET", "POST"],
-        credentials: true
-    }
-});
-//? io:- a whole server and socket:- a sigle user and inside that callback fire when the new connection was created
-
-//* Setup Socket.io chat routes
+//* Passing IO Instanse to chat routes
 setupChatRoutes(io);
 
 // const PORT = process.env.SOCKET_IO_PORT || 3007
